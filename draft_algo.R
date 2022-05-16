c137b5c936e1d4d65dda4d50e2f2c16b582838c4
nattydeacs <natdeacon@gmail.com>
2022-05-16 09:30:53 -0400

    Adjusted probabilities

--- draft_algo.R
+++ draft_algo.R
@@ -51,6 +51,8 @@ full_func <- function(position, count){
       }
 }
 
+varianceGeneerator<-sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))
+x<-simulateStandardDraft("2021", 2)
 #function to simulate drafts using standard strategy
 simulateStandardDraft <- function(year, NumberOfSimulations) {
   draftPick <- c(1:160)
@@ -75,13 +77,13 @@ simulateStandardDraft <- function(year, NumberOfSimulations) {
         positionTable <- positionTable %>% mutate(is_pos_full = mapply(full_func, positionTable$Var1, positionTable$Freq))
         fullPositions <- c(as.vector(positionTable[positionTable$is_pos_full == "Yes", 1]), "K", "DST")
         remainingPlayersPosFilter <- filter(remainingPlayers, !(Position %in% fullPositions))
-        pick <- remainingPlayersPosFilter[1+round((sample(0:1, 1))*sqrt(val), 1), ]
+        pick <- remainingPlayersPosFilter[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
         draftOrder[val,c(4:7)] <- pick
         remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
       }
       else if (val>=100 & val<=140) {
         roster <- filter(draftOrder, teamPick == draftOrder[val, "teamPick"])
-        pick <- remainingPlayers[1+round((sample(0:1, 1))*sqrt(val), 1), ]
+        pick <- remainingPlayers[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
         draftOrder[val,c(4:7)] <- pick
         remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
       }
@@ -89,12 +91,12 @@ simulateStandardDraft <- function(year, NumberOfSimulations) {
         roster <- filter(draftOrder, teamPick == draftOrder[val, "teamPick"])
         if(!("DST" %in% roster$Position)) {
           DST <- filter(remainingPlayers, Position == "DST")
-          pick <- DST[1, ]
+          pick <- DST[1+sample(c(0:5),1), ]
           draftOrder[val,c(4:7)] <- pick
           remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
         }
         else{
-          pick <- remainingPlayers[1+sample(0:2, 1), ]
+          pick <- remainingPlayers[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
           draftOrder[val,c(4:7)] <- pick
           remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
         }
@@ -103,12 +105,12 @@ simulateStandardDraft <- function(year, NumberOfSimulations) {
         roster <- filter(draftOrder, teamPick == draftOrder[val, "teamPick"])
         if(!("K" %in% roster$Position)) {
           K <- filter(remainingPlayers, Position == "K")
-          pick <- K[1, ]
+          pick <- K[1+sample(c(0:5),1), ]
           draftOrder[val,c(4:7)] <- pick
           remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
         }
         else{
-          pick <- remainingPlayers[1+sample(0:2, 1), ]
+          pick <- remainingPlayers[1+1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
           draftOrder[val,c(4:7)] <- pick
           remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
         }  
@@ -123,6 +125,7 @@ simulateStandardDraft <- function(year, NumberOfSimulations) {
   return(returnme)
 }
 
+x<- simulate0RBDraft("2021", 2)
 #function to simulate drafts 0RB standard strategy
 simulate0RBDraft <- function(year, NumberOfSimulations) {
   ZeroTeam <- "team_1"
@@ -151,13 +154,13 @@ simulate0RBDraft <- function(year, NumberOfSimulations) {
             positionTable <- positionTable %>%  mutate(is_pos_full = mapply(full_func, positionTable$Var1, positionTable$Freq))
             fullPositions <- c(as.vector(positionTable[positionTable$is_pos_full == "Yes", 1]), "K", "DST")
             remainingPlayersPosFilter <- filter(remainingPlayers, !(Position %in% fullPositions))
-            pick <- remainingPlayersPosFilter[1+round((sample(0:1, 1))*sqrt(val), 1), ]
+            pick <- remainingPlayersPosFilter[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
             draftOrder[val,c(4:7)] <- pick
             remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
           }
           else if (val>=100 & val<=140) {
             roster <- filter(draftOrder, teamPick == draftOrder[val, "teamPick"])
-            pick <- remainingPlayers[1+round((sample(0:1, 1))*sqrt(val), 1), ]
+            pick <- remainingPlayers[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
             draftOrder[val,c(4:7)] <- pick
             remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
           }
@@ -165,12 +168,12 @@ simulate0RBDraft <- function(year, NumberOfSimulations) {
             roster <- filter(draftOrder, teamPick == draftOrder[val, "teamPick"])
             if(!("DST" %in% roster$Position)) {
               DST <- filter(remainingPlayers, Position == "DST")
-              pick <- DST[1, ]
+              pick <- DST[1+sample(c(0:5),1), ]
               draftOrder[val,c(4:7)] <- pick
               remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
             }
             else{
-              pick <- remainingPlayers[1+sample(0:2, 1), ]
+              pick <- remainingPlayers[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
               draftOrder[val,c(4:7)] <- pick
               remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
             }
@@ -179,12 +182,12 @@ simulate0RBDraft <- function(year, NumberOfSimulations) {
             roster <- filter(draftOrder, teamPick == draftOrder[val, "teamPick"])
             if(!("K" %in% roster$Position)) {
               K <- filter(remainingPlayers, Position == "K")
-              pick <- K[1, ]
+              pick <- K[1+sample(c(0:5),1), ]
               draftOrder[val,c(4:7)] <- pick
               remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
             }
             else{
-              pick <- remainingPlayers[1+sample(0:2, 1), ]
+              pick <- remainingPlayers[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
               draftOrder[val,c(4:7)] <- pick
               remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
             }  
@@ -193,7 +196,7 @@ simulate0RBDraft <- function(year, NumberOfSimulations) {
         else if (draftOrder[val,"teamPick"] == ZeroTeam){
           if (val<=40) {
             remainingPlayersSanRB<- filter(remainingPlayers, Position !="RB")
-            pick <- remainingPlayersSanRB[1+round((sample(0:1, 1))*sqrt(val), 1), ]
+            pick <- remainingPlayersSanRB[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
             draftOrder[val,c(4:7)] <- pick
             remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
           }
@@ -203,13 +206,13 @@ simulate0RBDraft <- function(year, NumberOfSimulations) {
             positionTable <- positionTable %>%  mutate(is_pos_full = mapply(full_func, positionTable$Var1, positionTable$Freq))
             fullPositions <- c(as.vector(positionTable[positionTable$is_pos_full == "Yes", 1]), "K", "DST")
             remainingPlayersPosFilter <- filter(remainingPlayers, !(Position %in% fullPositions))
-            pick <- remainingPlayersPosFilter[1+round((sample(0:1, 1))*sqrt(val), 1), ]
+            pick <- remainingPlayersPosFilter[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
             draftOrder[val,c(4:7)] <- pick
             remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
           }
           else if (val>=100 & val<=140) {
             roster <- filter(draftOrder, teamPick == draftOrder[val, "teamPick"])
-            pick <- remainingPlayers[1+round((sample(0:1, 1))*sqrt(val), 1), ]
+            pick <- remainingPlayers[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
             draftOrder[val,c(4:7)] <- pick
             remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
           }
@@ -217,12 +220,12 @@ simulate0RBDraft <- function(year, NumberOfSimulations) {
             roster <- filter(draftOrder, teamPick == draftOrder[val, "teamPick"])
             if(!("DST" %in% roster$Position)) {
               DST <- filter(remainingPlayers, Position == "DST")
-              pick <- DST[1, ]
+              pick <- DST[1+sample(c(0:5),1), ]
               draftOrder[val,c(4:7)] <- pick
               remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
             }
             else{
-              pick <- remainingPlayers[1+sample(0:2, 1), ]
+              pick <- remainingPlayers[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
               draftOrder[val,c(4:7)] <- pick
               remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
             }
@@ -231,12 +234,12 @@ simulate0RBDraft <- function(year, NumberOfSimulations) {
             roster <- filter(draftOrder, teamPick == draftOrder[val, "teamPick"])
             if(!("K" %in% roster$Position)) {
               K <- filter(remainingPlayers, Position == "K")
-              pick <- K[1, ]
+              pick <- K[1+sample(c(0:5),1), ]
               draftOrder[val,c(4:7)] <- pick
               remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
             }
             else{
-              pick <- remainingPlayers[1+sample(0:2, 1), ]
+              pick <- remainingPlayers[1+round(sample(c(0,0.5,1), size =1, prob = c(0.6, 0.3, 0.1))*sqrt(val), 0), ]
               draftOrder[val,c(4:7)] <- pick
               remainingPlayers <- filter(remainingPlayers, PlayerID != pick$PlayerID)
             }
@@ -276,34 +279,10 @@ zero <- rbind(zero, simulate0RBDraft(year, simulations))
 print(year)
 }
 
-
-startTime <- Sys.time()
-simulate0RBDraft("2017", 1)
-endTime <- Sys.time()
-# prints recorded time
-print(endTime - startTime)
 data <- rbind(standard, zero)
-
-###############
-#clean up and aggregation
-###############
-
-dataClean <- data %>%
-  select(-c(Name.y, Position.y)) %>%
-  rename(name = Name.x) %>%
-  rename(rank_pre = Rank.x) %>%
-  rename(position = Position.x) %>%
-  rename(rank_post = Rank.y)
-
-dataAggregated <- dataClean %>%
-  group_by(teamPick, Strategy, simulation, yr) %>%
-  summarise(totalPoints = sum(FantasyPoints, na.rm = TRUE))
+write.csv(data, "0514sims.csv")
 
 
-
-#ggplot(allSimsClean, aes(x=totalPoints, y = teamPick))+
- # geom_density_ridges(scale = 0.9) #+
- # facet_wrap(~teamPick, nrow=3, ncol=4)
 
 
 
